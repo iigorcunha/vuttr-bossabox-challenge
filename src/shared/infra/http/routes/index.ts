43a4e7ce487e7316650c63c06128695d5bb9d4e7
Igor Cunha <igorccunha@hotmail.com>
2020-06-01 17:36:47 -0300

    fix: users url route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,6 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
-routes.use('users', usersRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
