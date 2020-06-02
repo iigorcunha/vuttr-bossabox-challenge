ea204a576b85113b2a5ede21f1e8c773c35a2d35
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 11:41:37 -0300

    fix: name pattern of Tools Repository

--- src/modules/tools/repositories/IToolsRepository.ts
+++ src/modules/tools/repositories/IToolsRepository.ts
@@ -1,7 +1,7 @@
 import Tool from '../infra/typeorm/entities/Tool';
 import ICreateToolDTO from '../dtos/ICreateToolDTO';
 
-export default interface IToolRepository {
+export default interface IToolsRepository {
   findByTag(tag: string): Promise<Tool | undefined>;
   create(data: ICreateToolDTO): Promise<Tool | undefined>;
   delete(id: string): Promise<void>;
