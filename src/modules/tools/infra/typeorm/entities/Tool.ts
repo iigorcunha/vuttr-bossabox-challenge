e3583595a02084fddcf3243ee6db542e899bab9f
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 11:29:02 -0300

    db: Create model of Tool

---
+++ src/modules/tools/infra/typeorm/entities/Tool.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('tools')
+class Tool {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  link: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Tool;
