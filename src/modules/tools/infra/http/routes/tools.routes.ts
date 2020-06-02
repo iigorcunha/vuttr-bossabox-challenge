04792569bd4bf09ca9596da1c70cb1c1ea37ab82
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 13:56:21 -0300

    routes: add tools create to local route

--- src/modules/tools/infra/http/routes/tools.routes.ts
+++ src/modules/tools/infra/http/routes/tools.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import ToolsController from '../controllers/ToolsController';
 
 const toolsRouter = Router();
+
+const toolsController = new ToolsController();
+
+toolsRouter.post('/', toolsController.create);
 
 export default toolsRouter;
