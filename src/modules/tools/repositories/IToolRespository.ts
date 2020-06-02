7805a3e8887d13a4cdeddc3bc8c323b28d71d420
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 11:34:36 -0300

    typescript: Create Tool Repository Interface

---
+++ src/modules/tools/repositories/IToolRespository.ts
@@ -0,0 +1,8 @@
+import Tool from '../infra/typeorm/entities/Tool';
+import ICreateToolDTO from '../dtos/ICreateToolDTO';
+
+export default interface IToolRepository {
+  findByTag(tag: string): Promise<Tool | undefined>;
+  create(data: ICreateToolDTO): Promise<Tool | undefined>;
+  delete(id: string): Promise<void>;
+}
