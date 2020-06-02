4f32b9e08387ab68cf7897005e890c4d9dc65317
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 10:52:12 -0300

    config: express handle custom errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import AppError from '@shared/infra/errors/AppError';
 
 import routes from './routes';
 import '@shared/container';
@@ -16,6 +17,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3000, () => {
   console.log('listen on port 3000');
