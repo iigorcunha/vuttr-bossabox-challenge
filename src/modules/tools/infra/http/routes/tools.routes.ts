1a5ea0422a3511545541db29fd0aa36c5916e5db
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 13:40:21 -0300

    routes: add tools route inside module and at main file

---
+++ src/modules/tools/infra/http/routes/tools.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const toolsRouter = Router();
+
+export default toolsRouter;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import toolsRouter from '@modules/tools/infra/http/routes/tools.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/tools', toolsRouter);
 
 export default routes;
