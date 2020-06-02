53779a416028f579aee958244e8e3e33cf13ec92
Igor Cunha <igorccunha@hotmail.com>
2020-06-02 13:46:52 -0300

    config: add ToolsRepository to Dependency Injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IToolsRepository from '@modules/tools/repositories/IToolsRepository';
+import ToolsRepository from '@modules/tools/infra/typeorm/repositories/ToolsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<IToolsRepository>(
+  'ToolsRepository',
+  ToolsRepository
 );
